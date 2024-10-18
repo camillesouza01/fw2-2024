95eb1c3e413492398c99d04613208056eb879314
cami <camille.souza@estudante.ifms.edu.br>
2024-10-18 08:39:19 -0300

    Copiado do repositório da aula

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,13 @@
+import { Container } from "react-bootstrap";
 import Footer from "./modulos/footer";
 import Menu from "./modulos/navbar";
-import {Container, Row, Col} from 'react-bootstrap';
 
-function Index(){
+export default function Index() {
   return <div>
-     <Menu/>
-    Bem-vindos
+    <Menu />
     <Container>
-      <Row>
-        <Col className="bg-primary">1 of 2</Col>
-        <Col className="bg-success-subtle">2 of 2</Col>
-      </Row>
-      <Row>
-        <Col className="bg-info-subtle">1 of 3</Col>
-        <Col className=" bg-primary-subtle">2 of 3</Col>
-        <Col className=" bg-secondary-subtle">3 of 3</Col>
-      </Row>
+      Bem Vindos a disciplina
     </Container>
-    <Footer/>
-  </div>
+    <Footer />
+  </div>;
 }
-
-export default Index;
