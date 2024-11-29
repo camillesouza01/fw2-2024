0369eeca3c014f1e6d72d0fa907ff8d80d4a4032
cami <camille.souza@estudante.ifms.edu.br>
2024-11-29 11:50:13 +0000

    29/11 - aula

--- src/pages/pesportes.js
+++ src/pages/pesportes.js
@@ -2,17 +2,35 @@ import { Container } from "react-bootstrap";
 import Menu from "./modulos/navbar";
 import Footer from "./modulos/footer";
 
-export default function Pesportes(){
-    return<>
-     <Menu/>
+export default function Pesportes() {
+    return <>
+        <Menu />
         <Container>
             <Container className="border border-sm border-sucess rounded-pill text-center col-sm-6">
                 Lista de produtos de esportes
             </Container>
             <Container>
-                 Os produtos
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Produto</th>
+                            <th>Categoria</th>
+                            <th>Descrição</th>
+                            <th>Preço</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </Table>
             </Container>
         </Container>
-        <Footer/>
+        <Footer />
     </>
 }
